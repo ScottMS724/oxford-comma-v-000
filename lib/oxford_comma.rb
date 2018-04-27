0fae4d34f959a3107c47eb63415b58745d2056d1
Scott Silverman <scottms724@gmail.com>
2018-04-27 15:24:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,5 @@ def oxford_comma(array)
   if array.size = 1 
     array.join
     return array 
-  elsif array.size = 2 
-    array.join("and")
-    return array 
-  else
-    array.join(",")
-    array.join(-1, "and")
-    return array
   end
 end
