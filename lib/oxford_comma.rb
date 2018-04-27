fbaf9c252deac8a86b5df9b4945c923a7b22b299
Scott Silverman <scottms724@gmail.com>
2018-04-27 15:41:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.size == 2 
     array = array.join(" and ")
     return array
-  else 
+  elsif array.size == 3
     array = array.join(", ")
     array.insert(-1, "and")
     return array
